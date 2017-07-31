6d18e3deecc19bd4439a6c4261546d1176a9e355
Giorgi Koguashvili <kogo27@gmail.com>
2017-07-31 10:43:14 +0400

    added groupElems impl

--- 3.1.3,4,6,8,12.hs
+++ 3.1.3,4,6,8,12.hs
@@ -89,3 +89,26 @@ sum3 [] (x:as) [] = x : (sum3 [] as [])
 sum3 (x:as) [] [] = x : (sum3 as [] [])
 sum3 (x:as) (y:bs) (z:cs) = 
     (x + y + z) : (sum3 as bs cs)
+
+
+{-
+Напишите функцию groupElems которая группирует одинаковые элементы в списке (если они идут подряд) и возвращает список таких групп.
+
+GHCi> groupElems []
+[]
+GHCi> groupElems [1,2]
+[[1],[2]]
+GHCi> groupElems [1,2,2,2,4]
+[[1],[2,2,2],[4]]
+GHCi> groupElems [1,2,3,2,4]
+[[1],[2],[3],[2],[4]]
+Разрешается использовать только функции, доступные из библиотеки Prelude.
+-}
+
+groupElems :: Eq a => [a] -> [[a]]
+groupElems [] = []
+groupElems [a] = [a] : []
+groupElems (a:as) = 
+    let (:) ((:) b bs) bss = groupElems as
+    in
+        if a == b then (a:b:bs):bss else (a:[]):((b:bs):bss)
