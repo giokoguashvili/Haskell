f34a6d39363a75d3766525f28ab2ae4a07bcdf83
Giorgi Koguashvili <kogo27@gmail.com>
2017-08-12 17:27:14 +0400

    added type class example in oop

--- oop.hs
+++ oop.hs
@@ -3,13 +3,24 @@ module Demo where
 data Person = Person { firstName::String, lastName::String, age::Int } deriving Show
 
 
-fullName :: Person -> ((a,a) -> String)
-fullName p =
-    let f (,) = firstName p
-    in f
+-- fullName :: Person -> ((a,a) -> String)
+-- fullName p =
+--     let f (,) = firstName p
+--     in f
 
 
 
 infixl 1 &
 (&) :: a -> (a -> b) -> b
 x & f = f x
+
+{-
+TypeClass
+-}
+class ClassType box where
+    doSomething :: (a,b) -> (box a) b
+
+data XType a b = XData {first::a, second::b} deriving Show
+
+instance ClassType XType where
+    doSomething (a, b) = XData a b
