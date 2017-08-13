4789950bd2d6c636e674804a2b730ab60eeeccad
Giorgi Koguashvili <kogo27@gmail.com>
2017-08-13 22:07:04 +0400

    added Monoid Maybe' solution

--- Monoid.hs
+++ Monoid.hs
@@ -23,3 +23,18 @@ instance Monoid Xor where
     mempty = Xor False
     mappend a b   | getXor a == True && getXor b == True = Xor False 
                   | otherwise = Xor ((getXor a) || (getXor b))
+
+
+{-
+Реализуйте представителя класса типов Monoid для Maybe' a так, чтобы mempty 
+не был равен Maybe' Nothing. Нельзя накладывать никаких дополнительных ограничений на тип a, 
+кроме указанных в условии.
+-}
+
+newtype Maybe' a = Maybe' { getMaybe :: Maybe a } deriving (Eq,Show)
+
+instance Monoid a => Monoid (Maybe' a) where
+    mempty = Maybe' $ Just mempty
+    mappend _ (Maybe' (Nothing)) = Maybe' $ Nothing
+    mappend (Maybe' (Nothing)) _ = Maybe' $ Nothing
+    mappend (Maybe' x) (Maybe' y) = Maybe' $ mappend x y
