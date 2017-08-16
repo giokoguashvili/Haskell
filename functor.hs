98bc3e56745f5f8af3ede37f5da39e572f1c85ed
Giorgi Koguashvili <kogo27@gmail.com>
2017-08-16 18:01:48 +0400

    added Tree Functor impl

--- functor.hs
+++ functor.hs
@@ -32,3 +32,25 @@ data GeomPrimitive a = Point (Point3D a) | LineSegment (Point3D a) (Point3D a)
 instance Functor GeomPrimitive where
     fmap f (LineSegment a b) = LineSegment (fmap f a) (fmap f b)
     fmap f (Point a) = Point $ fmap f a
+
+
+{-
+
+Определите представителя класса Functor для бинарного дерева, в каждом узле которого хранятся элементы типа Maybe:
+
+data Tree a = Leaf (Maybe a) | Branch (Tree a) (Maybe a) (Tree a) deriving Show
+
+GHCi> words <$> Leaf Nothing
+Leaf Nothing
+
+GHCi> words <$> Leaf (Just "a b")
+Leaf (Just ["a","b"]
+
+-}
+
+data Tree a = Leaf (Maybe a) | Branch (Tree a) (Maybe a) (Tree a) deriving Show
+instance Functor Tree where
+    fmap f (Leaf Nothing) = Leaf Nothing
+    fmap f (Leaf (Just a)) = Leaf (Just (f a))
+    fmap f (Branch l (Nothing) r) = Branch (fmap f l) Nothing (fmap f r)
+    fmap f (Branch l (Just a) r) = Branch (fmap f l) (Just (f a)) (fmap f r)
