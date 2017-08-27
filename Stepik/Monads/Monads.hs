090d24c03949b3e1066ab0a9364897ff7ce07c72
Giorgi Koguashvili <kogo27@gmail.com>
2017-08-27 18:26:55 +0400

    commi

--- Stepik/Monads/Monads.hs
+++ Stepik/Monads/Monads.hs
@@ -216,3 +216,29 @@ list'' =
     [1,2,3] >>= (\x ->
     [4,5,6] >>= (\y ->
     return (x,y)))
+
+lst = [(x,y) | x <- [1,2,3], y <- [1,2], x /= y]
+
+lst' = do
+    x <- [1,2,3]
+    y <- [1,2]
+    True <- return (x/=y)
+    return (x,y)
+
+lst'' = 
+    [1,2,3]         >>= (\x ->
+    [1,2]           >>= (\y ->
+    return (x/=y)   >>= (\b ->
+    case b of 
+        True    -> return (x,y)
+        _       -> [])))
+
+lst''' = do
+    x <- [1,2,3]
+    y <- [1,2]
+    if x/=y then
+        [True]
+    else
+        []
+    return (x,y)
+        
