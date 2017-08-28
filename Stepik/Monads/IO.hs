1776ae9fc8367c4dd681f51fb030326541888b86
Giorgi Koguashvili <kogo27@gmail.com>
2017-08-28 14:53:34 +0400

    added IO explenation

--- Stepik/Monads/IO.hs
+++ Stepik/Monads/IO.hs
@@ -1,3 +1,52 @@
 module Demo where
 
+    {-
+        https://wiki.haskell.org/IO_inside
+    -}
+
+    {-
+    newtype IO a = IO (RealWord -> (RealWord, a))
+    -}
+
+    main = getLine >>= (\str -> putStrLn $ str ++ "!") 
+
+    {-
+        1.
+            getchar :: Char
+            get2chars = [getChar, getChar]
+        
+        2.
+            getchar :: Int -> Char
+            get2chars :: Int -> String
+
+            get2chars _ = [getchar 1, getchar 2]
+
+        3.
+            getchar :: Int -> (Char, Int)
+            get2chars _ = [a,b] 
+                        where 
+                                (a,i) = getchar 1
+                                (b,_) = getchar i
+
+        4.
+            get2chars i0 = [a,b]
+                        where 
+                            (a,i1) = getchar i0
+                            (b,i2) = getchar i1
+            get4chars = [get2chars 1, get2chars 2] -- same problem
+
+        5.
+            get2chars :: Int -> (String, Int)
+            get4chars i0 = (a++b)
+                        where
+                            (a,i1) = get2char i0
+                            (b,i2) = get2char i1
+        
+        6.
+            get2chars :: Int -> (String, Int)
+            get2chars i0 = ([a,b], i2)
+                    where
+                        (a,i1) = getchar i0
+                        (b,i2) = getchar i1
     
+    -}
